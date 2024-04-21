28629f44d63f71c47227f6f948cb595ffa2d4299
PedroCarvalho <pedro2vianna@gmail.com>
2024-04-21 14:42:47 -0300

    funções de listas

--- ED/PraticaListas.c
+++ ED/PraticaListas.c
@@ -1,7 +1,9 @@
 
 
 #include<stdio.h>
-#define max 100
+#define max 10
+#define fantasma '$'
+#define sinal 0
 
 typedef struct{
 	int tamanho;
@@ -9,25 +11,36 @@ typedef struct{
 }Lista;
 
 Lista criarLista();
+
 Lista add(Lista, char);
-void imprimir(Lista);
+
+int obterTamanho(Lista);
+
+void mostrarLista(Lista);
+
+char obterElemento(Lista, int);
+
+Lista inserirLista(Lista, char);
 
 int main(){
 	
 	Lista L;
 	L = criarLista();
-	char y = 'y';
 	
-	L = add(L, 'l');
-	L = add(L, 'i');
-	L = add(L, 's');
+	mostrarLista(L);
+	
+	L = add(L, 'a');
 	L = add(L, 'b');
+	L = add(L, 'c');
+	L = add(L, 'd');
 	L = add(L, 'e');
-	L = add(L, 't');
-	
-	L = add(L, y);
-		
-	imprimir(L);
+	L = add(L, 'f');
+	L = add(L, 'g');
+	L = add(L, 'h');
+	L = add(L, 'i');
+	L = add(L, 'j');
+
+	mostrarLista(L);
 	
 	return 0;
 }
@@ -35,32 +48,60 @@ int main(){
 Lista criarLista(){
 	Lista b;
 	b.tamanho = 0;
+	b.elemento[0] = max - 1;
 	return b;
 }
 
-Lista add(Lista L, char y){
-	int i, n;
+Lista add(Lista L, char y){ //adiona um elemento do tipo caracter na lista caso não seja repetido
+	int i;
 	i = 0;
-	n = L.tamanho;
-	
-	if(n == 0){
-		n = n + 1;
-		L.elemento[n] = y;
-	}else{
-		while(L.elemento[i] != y && i < n){
+
+	if(L.tamanho+1 < max){
+		while(L.elemento[i] != y && i < L.tamanho)
 			i++;
-		}
-		if(i == n){
-			n = n + 1;
-			L.elemento[n] = y;
+		if(i == L.tamanho){
+			L.tamanho++;
+			L.elemento[L.tamanho] = y;
 		}
 	}
-	
 	return L;
 }
-void imprimir(Lista L) {
-	for (int i = 0; i < L.tamanho; i++) {
-		printf("%c", L.elemento[i]);
+
+int obterTamanho(Lista a){
+	int tam;
+	tam = a.tamanho;
+	return tam;
+}
+
+char obterElemento(Lista a, int p){
+	int ch;
+	int tam;
+	tam = a.tamanho;
+	if ((p >= 1) && (p <= tam)) 
+		ch = a.elemento[p];
+	else 
+		ch = fantasma; 
+	return ch;
+}
+
+void mostrarLista(Lista a){
+	int i;
+	int n;
+	n = a.tamanho;
+	if(n == 0){
+		printf("\nLista vazia\n");
+	}else{
+		for(i = 1; i <= n; i++)
+			printf("\n%c", a.elemento[i]);
 	}
-	printf("\n%d", L.tamanho);
+	printf("\n");
+}
+
+Lista inserirLista(Lista a, char ch){
+	int k, p;
+	k = a.tamanho;
+	p = k + 1;
+	a.elemento[p] = ch;
+	a.tamanho = p; 
+	return a;
 }
